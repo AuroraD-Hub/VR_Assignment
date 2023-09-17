1513f69c154cf847904d8c29461570968ec7ffc1
AuroraD-Hub <aurora99durante@gmail.com>
2023-09-17 12:07:41 +0200

    flight integration

--- scripts/flight_prova.py
+++ scripts/flight_prova.py
@@ -2,82 +2,88 @@
 
 import rospy
 import airsim
+import sys
+import os
+from os.path import dirname, realpath
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
 import json
 
 def move_drone():
-    # Inizializza il nodo ROS
-    rospy.init_node('drone_controller', anonymous=True)
+	# Inizializza il nodo ROS
+	rospy.init_node('drone_controller', anonymous=True)
 
-    # Inizializza il client di AirSim
-    client = airsim.MultirotorClient(ip="172.28.16.1", port=41451)
-    client.confirmConnection()
-    print("Connessione avvenuta")
-    
-    client.enableApiControl(True)
-    
-    client.armDisarm(True)
-    print("Drone armato")
-    
-    client.takeoffAsync().join()
-    print("Drone decollato")
+	# Inizializza il client di AirSim
+	#host = rospy.get_param("/launcher/host") # launch param
+	client = airsim.MultirotorClient(ip='172.23.32.1', port=41451)
+	client.confirmConnection()    
+	client.enableApiControl(True)    
+	client.armDisarm(True)
+	client.takeoffAsync().join()
 
-    # Ottieni la posizione corrente del drone
-    drone_position = client.getMultirotorState().kinematics_estimated.position
-    print("Drone position: ", drone_position)
+	# Ottieni la posizione corrente del drone
+	drone_position = client.getMultirotorState().kinematics_estimated.position
 
-    # Definisci una serie di waypoint
-    waypoints = [
-        airsim.Vector3r(drone_position.x_val + 12.0, drone_position.y_val, drone_position.z_val),
-        airsim.Vector3r(drone_position.x_val + 22.0, drone_position.y_val, drone_position.z_val),
-        airsim.Vector3r(drone_position.x_val + 32.0, drone_position.y_val, drone_position.z_val),
-        airsim.Vector3r(drone_position.x_val + 42.0, drone_position.y_val, drone_position.z_val),
-        airsim.Vector3r(drone_position.x_val + 90.0, drone_position.y_val - 10.0, drone_position.z_val)
-    ]
+	# Definisci una serie di waypoint
+	#waypoints = [
+	#    airsim.Vector3r(drone_position.x_val + 12.0, drone_position.y_val, drone_position.z_val),
+	#    airsim.Vector3r(drone_position.x_val + 22.0, drone_position.y_val, drone_position.z_val),
+	#    airsim.Vector3r(drone_position.x_val + 32.0, drone_position.y_val, drone_position.z_val),
+	#    airsim.Vector3r(drone_position.x_val + 42.0, drone_position.y_val, drone_position.z_val),
+	#    airsim.Vector3r(drone_position.x_val + 90.0, drone_position.y_val - 10.0, drone_position.z_val)
+	#]
+	path = dirname(dirname(realpath(__file__)))
+	path = path + '/graphs/drone_graph.json'
+	with open(path) as drone_path:
+		data = json.load(drone_path)
 
-    for idx, waypoint in enumerate(waypoints):
-        # Sposta il drone al waypoint corrente
-        client.moveToPositionAsync(waypoint.x_val, waypoint.y_val, waypoint.z_val, 1.0).join()
-        print(f"Drone posizione desiderata: {waypoint}")
+	waypoints = []
+	for i in range(0,4):
+		waypoints.append(airsim.Vector3r(data["nodes"][f"p{i}"]["position"][0],data["nodes"][f"p{i}"]["position"][1],data["nodes"][f"p{i}"]["position"][2]))
+		
+	print("Waypoint: ",waypoints)
 
-        # Ottieni la posizione corrente dopo il raggiungimento del waypoint
-        drone_position_after_waypoint = client.getMultirotorState().kinematics_estimated.position
-        print(f"Drone posizione dopo waypoint {idx}: {drone_position_after_waypoint}")
+	with open(path, "w") as save_path:
+		json.dump(data, save_path, indent=2)
 
-        # Apri il file JSON esistente e carica i dati
-        json_file_path = '/mnt/d/VR_Assignment/Assignment/SavedData/PollutionData.json'  # Sostituisci con il percorso effettivo
-        with open(json_file_path, 'r') as json_file:
-            data = json.load(json_file)
+	for idx, waypoint in enumerate(waypoints):
+		# Sposta il drone al waypoint corrente
+		client.moveToPositionAsync(waypoint.x_val, waypoint.y_val, waypoint.z_val, 1.0).join()
 
-        # Trova l'ultima sezione "PM25"
-        last_pm25_section = None
-        for section in reversed(data):
-            if "PM25" in section:
-                last_pm25_section = section
-                break
+	# Ottieni la posizione corrente dopo il raggiungimento del waypoint
+	#drone_position_after_waypoint = client.getMultirotorState().kinematics_estimated.position
+	#print(f"Drone posizione dopo waypoint {idx}: {drone_position_after_waypoint}")
 
-        # Aggiungi il campo "waypoint" con la posizione del drone
-        if last_pm25_section:
-            last_pm25_section["waypoint"] = {
-                "x": drone_position_after_waypoint.x_val,
-                "y": drone_position_after_waypoint.y_val,
-                "z": drone_position_after_waypoint.z_val
-            }
+	# Apri il file JSON esistente e carica i dati
+	#json_file_path = '/mnt/d/VR_Assignment/Assignment/SavedData/PollutionData.json'  # Sostituisci con il percorso effettivo
+	#with open(json_file_path, 'r') as json_file:
+	#    data = json.load(json_file)
 
-            # Salva il JSON aggiornato nel file
-            with open(json_file_path, 'w') as json_file:
-                json.dump(data, json_file, indent=4)
+	# Trova l'ultima sezione "PM25"
+	#last_pm25_section = None
+	#for section in reversed(data):
+	#    if "PM25" in section:
+	#        last_pm25_section = section
+	#        break
 
-    client.hoverAsync().join()
-    print("Arresto il drone")
-     
-    client.landAsync().join()
-    print("Drone atterrato")
+	# Aggiungi il campo "waypoint" con la posizione del drone
+	#if last_pm25_section:
+	#    last_pm25_section["waypoint"] = {
+	#        "x": drone_position_after_waypoint.x_val,
+	#        "y": drone_position_after_waypoint.y_val,
+	#        "z": drone_position_after_waypoint.z_val
+	#    }
 
-    # Chiudi la connessione al client di AirSim
-    client.reset()
-    client.enableApiControl(False)
+	    # Salva il JSON aggiornato nel file
+	#    with open(json_file_path, 'w') as json_file:
+	#        json.dump(data, json_file, indent=4)
+
+	client.hoverAsync().join()     
+	client.landAsync().join()
+
+	# Chiudi la connessione al client di AirSim
+	client.reset()
+	client.enableApiControl(False)
 
 if __name__ == '__main__':
     try:
--- scripts/launcher.py
+++ scripts/launcher.py
@@ -53,20 +53,21 @@ def open_terminals():
 	command = f"gnome-terminal -e 'roslaunch airsim_ros_pkgs airsim_node.launch output:=screen host:={host}'"
 	wrapper = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 	
-	folder_path = dirname(dirname(dirname(realpath(__file__))))
-	folder_path = folder_path + '/VR4R_Assignment'
-	command = f"gnome-terminal --working-directory={folder_path} -e 'python run.py'"
+	#folder_path = dirname(dirname(dirname(realpath(__file__))))
+	#folder_path = folder_path + '/VR4R_Assignment'
+	#command = f"gnome-terminal --working-directory={folder_path} -e 'python run.py'"
+	command = "gnome-terminal -e 'rosrun VR_Assignment flight_prova.py'"
 	controller = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 	
-	input("Follow the instruction on drone controller terminal, then press any key here.")
+	#input("Follow the instruction on drone controller terminal, then press any key here.")
        
 	# Load JSON graph and path to follow
-	graph = dirname(dirname(realpath(__file__)))
-	graph = graph + '/graphs/drone_graph.json'
-	client_file = rospy.ServiceProxy('/graph_knowledge/load_graph', LoadCoverageGraph)
-	client_file.call(graph)
-	client_drone_path = rospy.ServiceProxy('/graph_knowledge/compute_path', ComputeCoveragePath)
-	client_drone_path.call('p0', 'p0')
+	#graph = dirname(dirname(realpath(__file__)))
+	#graph = graph + '/graphs/drone_graph.json'
+	#client_file = rospy.ServiceProxy('/graph_knowledge/load_graph', LoadCoverageGraph)
+	#client_file.call(graph)
+	#client_drone_path = rospy.ServiceProxy('/graph_knowledge/compute_path', ComputeCoveragePath)
+	#client_drone_path.call('p0', 'p0')
 	
 def close_terminals():
 	# Stop the Wrapper and close related terminal
